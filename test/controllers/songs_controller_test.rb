ddd1c2d198daffade9588b48ae5575c0010a2869
Cristian B <cris.bahena77@gmail.com>
2023-12-06 13:55:26 -0600

    added index and create methods

---
+++ test/controllers/songs_controller_test.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+
+class SongsControllerTest < ActionDispatch::IntegrationTest
+  test "index" do
+    get "/songs.json"
+    assert_response 200
+    data = JSON.parse(response.body)
+    assert_equal Song.count, data.length
+  end
+
+  test "create" do
+    assert_difference "Photo.count", 1 do
+      post "/photos.json", params: { name: "lake", width: 800, height: 600 }
+      assert_response 200
+    end
+  end
+end
