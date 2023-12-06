86add06dc6041712ce4af5de41f9b06ab103f748
Cristian B <cris.bahena77@gmail.com>
2023-12-06 13:57:22 -0600

    updated test for create

--- test/controllers/songs_controller_test.rb
+++ test/controllers/songs_controller_test.rb
@@ -9,8 +9,8 @@ class SongsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "create" do
-    assert_difference "Photo.count", 1 do
-      post "/photos.json", params: { name: "lake", width: 800, height: 600 }
+    assert_difference "Song.count", 1 do
+      post "/songs.json", params: { name: "lake", width: 800, height: 600 }
       assert_response 200
     end
   end
