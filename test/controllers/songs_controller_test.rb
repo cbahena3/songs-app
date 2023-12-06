c1afc8c6844aa13267d6f1051978c567dfef6b8f
Cristian B <cris.bahena77@gmail.com>
2023-12-06 14:04:53 -0600

    added show method and test

--- test/controllers/songs_controller_test.rb
+++ test/controllers/songs_controller_test.rb
@@ -14,4 +14,12 @@ class SongsControllerTest < ActionDispatch::IntegrationTest
       assert_response 200
     end
   end
+
+  test "show" do
+    get "/songs/#{Song.first.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal ["id", "title", "album", "artist", "year", "created_at", "updated_at"], data.keys
+  end
 end
